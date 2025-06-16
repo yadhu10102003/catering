acc5a89fc7374ba9f1f51daaf2ff38ef0b05417d
Vidhu <vidhuvishwak2004@gmail.com>
2025-06-16 10:58:54 +0530

    Initial commit

---
+++ src/frontend/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div style={{ marginTop: '150px' }}>
+            <div className='Banner'>
+                <h1>Silver Spoon Catering</h1>
+            </div>
+            <div className='Banner'>
+                <h3>"Where every event begins with elegance and ends with satisfaction."</h3>
+            </div>
+            <div className='desc'>
+                <p>At Silver Spoon Catering, we specialize in transforming meals into memorable experiences. Whether you're planning a luxurious wedding, a corporate gathering, or a private celebration, our team is dedicated to delivering impeccable service and gourmet cuisine tailored to your vision.
+                    With a passion for detail, fresh ingredients, and flawless presentation, we go beyond just catering — we curate culinary moments that delight every guest. From customized menus to seamless event coordination, Silver Spoon Catering ensures your occasion is not only successful, but unforgettable.
+                    Let us serve you excellence — one silver spoon at a time.</p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
